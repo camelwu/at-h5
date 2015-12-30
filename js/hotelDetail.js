f7e69c03afd8a78a4ccf41fe1538408426f19b2e
heyong841 <heyong@asiatravel.com>
2015-12-30 09:37:48 +0800

    1.酒店详情 2.房间详情

---
+++ js/hotelDetail.js
@@ -0,0 +1,39 @@
+/**
+ * Created by Andy on 2015/12/28.
+ */
+
+window.onload=function(){
+    if(window.localStorage){
+        var hotelMessage = window.localStorage.getItem('hotelMessage');
+    };
+    addContent(hotelMessage);
+    function addContent(hotelMessage){
+      var hotelDesc = document.querySelector('.s-content1');
+          hotelDesc.innerText =JSON.parse(hotelMessage)[0].HotelGenInfo.HotelDesc;
+
+/*
+       var scenicSpot = document.querySelector('.s-p3');
+        //  scenicSpot.innerText =JSON.parse(hotelMessage)[0].HotelGenInfo.HotelDesc;
+
+        function getAllSpot(arg){
+            var str = '<ul class="s-ul1">';
+            for(var i=0;i<arg.length;i++){
+                str+='<li class="ul1-li"><p class="f-l">arg[i]['name']</p><p class="f-r">arg[i]['disdance']</p></li>';
+            }
+            str+=' </ul>';
+            return str;
+         }
+
+
+        function getFeature(arg){
+            var str = '<ul class="s-ul2">';
+            for(var i=0;i<arg.length;i++){
+                str+='<li class="ul2-li"><b class="s-icon3"></b><p class="s-p4">arg['featureIndex']</p></li>';
+            }
+            str+=' </ul>';
+            return str;
+        }
+*/
+
+    }
+}
