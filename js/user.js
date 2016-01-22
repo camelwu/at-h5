9abb768a9d9140ba91aa06ef7a02f68bc24d3279
wusongbo <wusongbo@asiatravel.com>
2016-01-22 14:58:34 +0800

    用户中心：时间选择滑块功能，第一版只是完成版，尚未做成通用；基本的滑动和样式切换功能实现。

--- js/user.js
+++ js/user.js
@@ -55,9 +55,92 @@ require.config({
 });
 
 require(['jquery','custom','vcm','vlm'], function($,custom,vcm,vlm) {
-	//console.log("dataReady="+vlm);
+	console.log($);
 	var viewer = new vlm();
 	viewer._init();
-
+	
+	 var special = jQuery.event.special,
+        uid1 = 'D' + (+new Date()),
+        uid2 = 'D' + (+new Date() + 1);
+ 
+    special.scrollstart = {
+        setup: function() {
+ 
+            var timer,
+                handler =  function(evt) {
+ 
+                    var _self = this,
+                        _args = arguments;
+ 
+                    if (timer) {
+                        clearTimeout(timer);
+                    } else {
+                        evt.type = 'scrollstart';
+                        jQuery.event.handle.apply(_self, _args);
+                    }
+ 
+                    timer = setTimeout( function(){
+                        timer = null;
+                    }, special.scrollstop.latency);
+ 
+                };
+ 
+            jQuery(this).bind('scroll', handler).data(uid1, handler);
+ 
+        },
+        teardown: function(){
+            jQuery(this).unbind( 'scroll', jQuery(this).data(uid1) );
+        }
+    };
+ 
+    special.scrollstop = {
+        latency: 300,
+        setup: function() {
+ 
+            var timer,
+                    handler = function(evt) {
+ 
+                    var _self = this,
+                        _args = arguments;
+ 
+                    if (timer) {
+                        clearTimeout(timer);
+                    }
+ 
+                    timer = setTimeout( function(){
+ 
+                        timer = null;
+                        evt.type = 'scrollstop';
+                        jQuery.event.handle.apply(_self, _args);
+ 
+                    }, special.scrollstop.latency);
+ 
+                };
+ 
+            jQuery(this).bind('scroll', handler).data(uid2, handler);
+ 
+        },
+        teardown: function() {
+            jQuery(this).unbind( 'scroll', jQuery(this).data(uid2) );
+        }
+    };
+    $(".sel-time").bind("scrollstart",function(e){
+    	
+    });
+    $(".sel-time").bind("scrollstop",function(e){
+		var that = this,obj=$(this),
+		posY = this.scrollTop,
+		p = parseInt(posY/39),
+		h = posY/39 - p <=0.5?p*39:(p+1)*39;
+		if(posY/39 - p >0.5){
+			p++;
+		}
+		console.log(posY+","+p+","+h+"=="+(posY/39 - p)+"="+(posY/39 - p<=0.5));
+		$(this).animate({scrollTop:h} ,300);
+		$(this).children("span").removeAttr("style");
+		$(this).children("span").eq((p+2)).css({"color":"#484848","font-size":"2.0rem"});
+	});
 });
 //define
+
+//AMD CMD
