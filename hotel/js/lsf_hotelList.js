74c5633816feda74a87e77a44d4afb7792a8881c
liushaofei <liushaofei@asiatravel.com>
2016-02-19 12:13:29 +0800

    价格改为显示人民币

--- hotel/js/lsf_hotelList.js
+++ hotel/js/lsf_hotelList.js
@@ -422,7 +422,7 @@ function styleChange(id,mytext){
                     '<span style="color:#999999;font-size:1rem;">分/'+data[i].hotelReviewCount+'人点评</span>'+
                     '<p class="hl_price">'+
                     '<span style="font-size:0.8rem;color:#fe4716;">￥</span>'+
-                    '<span style="font-size:2rem;font-weight: 600;color:#fe4716;">'+parseFloat(data[i].avgPrice).toFixed(2)+'</span>'+
+                    '<span style="font-size:2rem;font-weight: 600;color:#fe4716;">'+parseFloat(data[i].avgPriceCNY).toFixed(2)+'</span>'+
                     '<span style="font-size:1.2rem;color:#999999;">起</span>'+
                     '</p>'+
                     '</div>'+
