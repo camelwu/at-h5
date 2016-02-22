6dd2d24a8e7ea151b4cc2201e491be456d7440cd
wusongbo <wusongbo@asiatravel.com>
2016-02-22 17:02:36 +0800

    完善选择乘机&联系人

--- js/lib/vlm.js
+++ js/lib/vlm.js
@@ -16,7 +16,7 @@
 			var reg = new RegExp("(^|&)" + str + "=([^&]*)(&|$)");
 			var r = window.location.search.substr(1).match(reg);
 			if (r != null)
-				return unescape(r[2]);
+				return decodeURIComponent(r[2]);
 			return null;
 		}, parseUrlPara = function(url, isEncode) {
 			var isEncode = isEncode || false;
