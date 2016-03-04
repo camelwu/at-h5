a7ad38434a2df98a90add50a3479a86eef1dd5ca
qizhenzhen@asiatravel.com <haiou123456>
2016-03-04 19:01:39 +0800

    下单js

--- tour/js/fill_order.js
+++ tour/js/fill_order.js
@@ -16,10 +16,11 @@
     package_detail();
 
     //localStorage.Info
-    var jsonPackage=localStorage.info;
+    var jsonPackage=JSON.parse(localStorage.info);
     console.log(jsonPackage);
 
     //有无航班信息
+    localStorage.BookingFormInfo=1;
     if( ! localStorage.BookingFormInfo)
     {
         $('#flight-air').remove();
@@ -32,16 +33,23 @@
 
 
     //根据房间数创建房间信息
-    var roomNumber=localStorage.roomNumber=2;
+    var roomNumber=jsonPackage.roomNum;
     for(var i=0;i<roomNumber; i++)
     {
         var oRoom=document.createElement('div');
         oRoom.className='per_data';
         oRoom.innerHTML='<span class="title"><i>房间'+(i+1)+'</i></span>'
-            +'<ul>'
-            +'<section class="li_section_box">'
-            +'<li>'
-            +'<span class="list_tit">成人1：</span>'
+            +'<ul></ul>'
+        $('#per-room-wrap')[0].appendChild(oRoom);
+        var oRoomNum=document.querySelectorAll('.per_data');
+
+        //每个房间成人数
+        for(var k=0;k<jsonPackage.eveAdultNum[i];k++)
+        {
+            var oSection=document.createElement('section');
+            oSection.className='li_section_box';
+            oSection.innerHTML='<li>'
+            +'<span class="list_tit">成人'+(k+1)+'：</span>'
             +'<b class="add_icon"><a href="javascript:;" class="add-contact"></a></b></span>'
             +'</li>'
             +'<li class="trave-li">'
@@ -53,23 +61,32 @@
             +'<li>'
             +'<span class="list_tit">护照：</span>'
             +' <span class="list_con"><input class="list_inp" type="text" placeholder="2088833" /></span>'
-            +'</li>'
-            +'</section>'
-            +'<section class="li_section_box">'
-            +'<li>'
-            +'<span class="list_tit">儿童1：</span>'
-            +'<b class="add_icon"><a href="javascript:;" class="add-contact"></a></b></span>'
-            +'</li>'
-            +'<li class="trave-li">'
-            +'<span class="list_tit2">姓：</span>'
-            +'<span class="list_con2"><input class="list_inp2" type="text" placeholder="Zhang" /></span>'
-            +'<span class="list_tit2">名：</span>'
-            +'<span class="list_con2"><input class="list_inp2" type="text" placeholder="Xiaohua" /></span>'
-            +'</li>'
-            +'</section>'
-            +'</ul>'
-        $('#per-room-wrap')[0].appendChild(oRoom);
+            +'</li>';
+            oRoomNum[i].querySelector('ul').appendChild(oSection);
+
+        }
+
+
+        for(var j=0;j<jsonPackage.eveChildNum[i]; j++)
+        {
+            var oSection=document.createElement('section');
+            oSection.className='li_section_box';
+            oSection.innerHTML='<li>'
+                +'<span class="list_tit">儿童'+(j+1)+'：</span>'
+                +'<b class="add_icon"><a href="javascript:;" class="add-contact"></a></b></span>'
+                +'</li>'
+                +'<li class="trave-li">'
+                +'<span class="list_tit2">姓：</span>'
+                +'<span class="list_con2"><input class="list_inp2" type="text" placeholder="Zhang" /></span>'
+                +'<span class="list_tit2">名：</span>'
+                +'<span class="list_con2"><input class="list_inp2" type="text" placeholder="Xiaohua" /></span>'
+                +'</li>'
+                +'</section>';
+            oRoomNum[i].querySelector('ul').appendChild(oSection);
+        }
+
     }
+
 
     //添加联系人
     $('.add-contact').click(function(){
@@ -101,17 +118,8 @@
     function sentPackage(obj){
         obj.onclick=function(){
             this.style.backgroundColor='#ff9313';
-            var roomNum=document.querySelectorAll('.per_data');
-            for(var i=0;i<roomNum.length; i++)
-            {
-                var inputAdult=roomNum[i].querySelectorAll('.list_con2 input');
-                var lastNameAdu=inputAdult[0].value;
-                var firstNameAdu=inputAdult[1].value;
-                var passport=roomNum[i].querySelectorAll('.list_con input').value;
-                var lastNameChi=inputAdult[2].value;
-                var firstNameChi=inputAdult[3].value;
-            }
 
+            var roomNum=document.querySelectorAll('.per_data');
             //联系人信息
             var conInput=document.querySelectorAll('#personal_data .list_inp2');
             var conLasName=conInput[0].value;
@@ -119,17 +127,8 @@
             var conPhone=conInput[2].value;
             var conEmail=conInput[3].value;
 
-            //航班信息
-            if($('#flight-air')[0]){
-                var departNum=document.querySelector('#content3 .input_flight input').value;
-                var departTime=document.querySelector('#content3_CheckInDate').value;
-                var arriveNum=document.querySelector('#content4 .input_flight input').value;
-                var arriveTime=document.querySelector('#content4_CheckInDate').value;
-            }
-
             //存联系人信息
             function setContact(){
-
                 var oLast=$('#list-lastname').val();
                 var oFir=$('#list-firstname').val();
                 var oConTel=$('#list_con_tel').val();
@@ -140,76 +139,43 @@
             };
             setContact();
 
+            //航班信息
+            if($('#flight-air')[0]){
+                var departFlightNo=document.querySelector('#content3 .input_flight input').value;
+                var departDateTime=document.querySelector('#content3_CheckInDate').value;
+                var arrivalFlightNo=document.querySelector('#content4 .input_flight input').value;
+                var arrivalDateTime=document.querySelector('#content4_CheckInDate').value;
+            }
+            //总价
+            var totalSale=document.querySelector('.all_num i').innerHTML;
 
             var Parmeters={
-
                 "Parameters": {
                     "PackageID": ""+localStorage.packageID+"",
-                    "CheckinDate": "2016-03-08T00:00:00",
-                    "CheckoutDate": "2016-03-10T00:00:00",
+                    "CheckinDate": ""+jsonPackage.checkInDate+"",
+                    "CheckoutDate": ""+jsonPackage.checkOutDate+"",
                     "HotelID": "30",
                     "RoomID": "77501",
                     "RoomDetails": [
                         {
-                            "Adult": "2"
+                            "Adult": ""+jsonPackage.adultNum+""
                         }
                     ],
-                    "Tours": [
-                        {
-                            "TourID": "137",
-                            "TravelDate": "2016-03-09T00:00:00",
-                            "TourSession": "None"
-                        },
-                        {
-                            "TourID": "166",
-                            "TravelDate": "2016-03-09T00:00:00",
-                            "TourSession": "None"
-                        }
-                    ],
-                    "FlightDetails": {
-                        "ArrivalFlightNo": "JT678",
-                        "ArrivalDateTime": "2016-02-02T00:00:00",
-                        "DepartFlightNo": "JT878",
-                        "DepartDateTime": "2016-02-05T00:00:00"
-                    },
 
-//                            "FlightDetails": {
-//                                "ArrivalFlightNo": ""+arriveNum+"",
-//                                "ArrivalDateTime": ""+arriveTime+"",
-//                                "DepartFlightNo": ""+departNum+"",
-//                                "DepartDateTime": ""+departTime+""
-//                            },
-                    "Travelers": [
-                        {
-                            "RoomSeqNo": "1",
-                            "TravelerType": "Adult",
-                            "Salutation": "Mr",
-                            "FirstName": "Kim",
-                            "LastName": "Pin",
-                            "NationalityCode": "SG"
-                        },
-                        {
-                            "RoomSeqNo": "1",
-                            "TravelerType": "Adult",
-                            "Salutation": "Mr",
-                            "FirstName": "Han",
-                            "LastName": "Pin"
-                        }
-                    ],
                     "ContactDetails": {
                         "Salutation": "Mr",
-                        "FirstName": "Kim",
-                        "LastName": "Pin",
-                        "Email": "test@asiatravel.com",
+                        "FirstName": ""+conFirName+"",
+                        "LastName": ""+conLasName+"",
+                        "Email": ""+conEmail+"",
                         "ContactNo": {
                             "CountryCode": "65",
-                            "PhoneNo": "7678767"
+                            "PhoneNo": ""+conPhone+""
                         },
                         "MemberID": "6789"
                     },
                     "ChargeDetails": {
                         "CurrencyCode": "CNY",
-                        "TotalPrice": "2610.00"
+                        "TotalPrice": ""+totalSale+""
                     }
                 },
                 "Method": null,
@@ -217,84 +183,57 @@
                 "Code": "0204"
             };
 
-//            var Parmeters={
-//
-//                "Parameters": {
-//                    "PackageID": "159",
-//                    "CheckinDate": "2016-03-08T00:00:00",
-//                    "CheckoutDate": "2016-03-10T00:00:00",
-//                    "HotelID": "30",
-//                    "RoomID": "77501",
-//                    "RoomDetails": [
-//                        {
-//                            "Adult": "2"
-//                        }
-//                    ],
-//                    "Tours": [
-//                        {
-//                            "TourID": "137",
-//                            "TravelDate": "2016-03-09T00:00:00",
-//                            "TourSession": "None"
-//                        },
-//                        {
-//                            "TourID": "166",
-//                            "TravelDate": "2016-03-09T00:00:00",
-//                            "TourSession": "None"
-//                        }
-//                    ],
-//                    "FlightDetails": {
-//                        "ArrivalFlightNo": "JT678",
-//                        "ArrivalDateTime": "2016-02-02T00:00:00",
-//                        "DepartFlightNo": "JT878",
-//                        "DepartDateTime": "2016-02-05T00:00:00"
-//                    },
-//
-////                            "FlightDetails": {
-////                                "ArrivalFlightNo": ""+arriveNum+"",
-////                                "ArrivalDateTime": ""+arriveTime+"",
-////                                "DepartFlightNo": ""+departNum+"",
-////                                "DepartDateTime": ""+departTime+""
-////                            },
-//                    "Travelers": [
-//                        {
-//                            "RoomSeqNo": "1",
-//                            "TravelerType": "Adult",
-//                            "Salutation": "Mr",
-//                            "FirstName": "Kim",
-//                            "LastName": "Pin",
-//                            "NationalityCode": "SG"
-//                        },
-//                        {
-//                            "RoomSeqNo": "1",
-//                            "TravelerType": "Adult",
-//                            "Salutation": "Mr",
-//                            "FirstName": "Han",
-//                            "LastName": "Pin"
-//                        }
-//                    ],
-//                    "ContactDetails": {
-//                        "Salutation": "Mr",
-//                        "FirstName": "Kim",
-//                        "LastName": "Pin",
-//                        "Email": "test@asiatravel.com",
-//                        "ContactNo": {
-//                            "CountryCode": "65",
-//                            "PhoneNo": "7678767"
-//                        },
-//                        "MemberID": "6789"
-//                    },
-//                    "ChargeDetails": {
-//                        "CurrencyCode": "CNY",
-//                        "TotalPrice": "2610.00"
-//                    }
-//                },
-//                "Method": null,
-//                "ForeEndType": 3,
-//                "Code": "0204"
-//            };
-            console.log(Parmeters);
+            //添加景点信息
+            var Tour=[];
+            for(var i=0;i<jsonPackage.tourList.length; i++)
+            {
+                var tour={};
+                tour.TourID=jsonPackage.tourList[0].tourId;
+                tour.TravelDate=jsonPackage.tourList[0].tourDate;
+                tour.TourSession="None";
+                Tour.push(tour);
+                Parmeters.Parameters.Tours=Tour;
+            }
+
+            //创建房间及添加旅客姓名等信息
+            var traveler=[];
+            for(var i=0;i<roomNum.length; i++)
+            {
+                var inputAdult=roomNum[i].querySelectorAll('.list_con2 input');
+                var lastNameAdu=inputAdult[0].value;
+                var firstNameAdu=inputAdult[1].value;
+                var passport=roomNum[i].querySelectorAll('.list_con input').value;
+                var lastNameChi=inputAdult[2].value;
+                var firstNameChi=inputAdult[3].value;
+
+                var tra={};
+                tra.RoomSeqNo=""+(i+1)+"";
+                tra.TravelerType="Adult";
+                tra.Salutation="Mr";
+                tra.FirstName=firstNameAdu;
+                tra.LastName=lastNameAdu;
+                tra.NationalityCode="Mr";
+                traveler.push(tra);
+                Parmeters.Parameters.Travelers=traveler;
+            }
+
+            //接机信息
+            if($('#flight-air')[0]){
+                var flight=[];
+                var fli={};
+                fli.ArrivalFlightNo=arrivalFlightNo;
+                fli.ArrivalDateTime=arrivalDateTime;
+                fli.DepartFlightNo=departFlightNo;
+                fli.DepartDateTime=departDateTime;
+                flight.push(fli);
+                Parmeters.Parameters.FlightDetails=flight;
+            }
+
+            console.log(Parmeters.Parameters.FlightDetails);
             vlm.loadJson("",JSON.stringify(Parmeters),package_back);
         }
+
+
 
         function package_back(ret){
             var json = eval('('+ret+')');
