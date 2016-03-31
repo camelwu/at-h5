fbb3d013c1c08c924a426d0004bfb877cd97621c
centos7 <liuwenshuang@local.asia>
2016-03-31 15:22:09 +0800

    20160331 release 1.0.1

---
+++ echo.php
@@ -0,0 +1,13 @@
+<?php
+$fileName="log.txt";
+
+if(file_exists($fileName)) {
+  echo("存在");  
+} else{
+  echo("不存在");  
+}
+
+$file = fopen("log.txt","w");  
+fwrite($file,date("Y-m-d H:i:s"));  
+fclose($file);
+?>
