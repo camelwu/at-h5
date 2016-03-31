22196d4fa4af95e57b7da9ff4e0838c0c118e7ca
Venson <liuwenshuang@asiatravel.com>
2016-03-31 17:34:48 +0800

    wenshuang echo

--- echo.php
+++ echo.php
@@ -1,4 +1,5 @@
 <?php
+
 $fileName="log.txt";
 
 if(file_exists($fileName)) {
@@ -7,10 +8,27 @@ if(file_exists($fileName)) {
   echo("不存在!");
 }
 
-$file = fopen("log.txt","w");  
-fwrite($file,date("Y-m-d H:i:s"));  
-fclose($file);
 
-system("ls",$result);
-print_r($result);
+$shell = <<<EOF
+$WEB_PATH = "/var/www/AT-H5-02";
+echo "Start deployment"
+cd \$WEB_PATH
+echo "pulling source code..."
+//git reset --hard origin/master
+//git clean -f
+git pull
+//git checkout master
+//echo "changing permissions..."
+//chown -R \$WEB_USER:\$WEB_GROUP \$WEB_PATH
+echo "Finished."
+EOF;
+
+file_put_contents('deploy.sh', $shell);
+$res = shell_exec("bash deploy.sh");
+print_r($res);
+echo "<br/>";
+
+$file = fopen("log.txt","w");
+fwrite($file,date("Y-m-d H:i:s"));
+fclose($file);
 ?>
