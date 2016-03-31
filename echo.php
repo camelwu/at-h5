a0c159e42b874002dfdfb4601b54f5c8a1ae050f
Venson <liuwenshuang@asiatravel.com>
2016-03-31 17:40:44 +0800

    wenshuang echo

--- echo.php
+++ echo.php
@@ -9,23 +9,10 @@ if(file_exists($fileName)) {
 }
 
 
-$shell = <<<EOF
 $WEB_PATH = "/var/www/AT-H5-02";
-echo "Start deployment"
-cd \$WEB_PATH
-echo "pulling source code..."
-//git reset --hard origin/master
-//git clean -f
-git pull
-//git checkout master
-//echo "changing permissions..."
-//chown -R \$WEB_USER:\$WEB_GROUP \$WEB_PATH
-echo "Finished."
-EOF;
-
-file_put_contents('deploy.sh', $shell);
-$res = shell_exec("bash deploy.sh");
-print_r($res);
+$command = "cd $WEB_PATH && git pull";
+$result = exec($command);
+print_r($result);
 echo "<br/>";
 
 $file = fopen("log.txt","w");
