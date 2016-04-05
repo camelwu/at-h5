927a8455ffb3a9eb9c39f5ae3633004982bf60b3
Venson <liuwenshuang@asiatravel.com>
2016-04-05 10:42:19 +0800

    wenshuang vlm.js and scenic_pub.js changed url 123.56.190.34:8888

--- js/lib/vlm.js
+++ js/lib/vlm.js
@@ -7,8 +7,8 @@
  * ver:1.1.1
  */
 (function(e, t) {"use strict";
-	var n = n || (function(n) {//123.56.190.34
-			var _api = "http://10.6.11.20:8888/api/GetServiceApiResult", lStorage = window.localStorage, sStorage = window.sessionStorage, basePath = basePath == undefined ? "http://" + window.location.host : basePath, menus = {
+	var n = n || (function(n) {//123.56.190.34 //10.6.11.20:8888
+			var _api = "http://123.56.190.34:8888/api/GetServiceApiResult", lStorage = window.localStorage, sStorage = window.sessionStorage, basePath = basePath == undefined ? "http://" + window.location.host : basePath, menus = {
 				home : ['首页', basePath],
 				find : ['目的地', basePath + '/building.html'],
 				user : ['我的', basePath + '/user/user.html']
