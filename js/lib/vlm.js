4d4fc32dc3ea21b1490995e4e0d1df11f353b12a
wusongbo <wusongbo@asiatravel.com>
2016-04-11 15:18:22 +0800

    数据请求地址修正

--- js/lib/vlm.js
+++ js/lib/vlm.js
@@ -9,7 +9,7 @@
 (function(e, t) {"use strict";
 
 	var n = n || (function(n) {//123.56.190.34:8888 //10.6.11.20:8888
-			var _api = "http://10.6.10.32/api/GetServiceApiResult", lStorage = window.localStorage, sStorage = window.sessionStorage, basePath = basePath == undefined ? "http://" + window.location.host : basePath, menus = {
+			var _api = "http://10.6.11.20:8888/api/GetServiceApiResult", lStorage = window.localStorage, sStorage = window.sessionStorage, basePath = basePath == undefined ? "http://" + window.location.host : basePath, menus = {
 				home : ['首页', basePath],
 				find : ['目的地', basePath + '/building.html'],
 				user : ['我的', basePath + '/user/user.html']
