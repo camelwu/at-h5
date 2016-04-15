965d9d849d88ba6c0b3c2363dbdc98d8c7f96b59
lichengjun <taoxirou@163.com>
2016-04-15 10:19:37 +0800

    H5-1008 酒店国际搜索时间默认T+2~T+3

--- hotel/js/lsf_hotel.js
+++ hotel/js/lsf_hotel.js
@@ -831,7 +831,8 @@ function inpChange(id, myText) {
 	var oDate2 = new Date(oDate.getFullYear(), oDate.getMonth(), oDate.getDate() + 3);
 	var beginDate = oDate1.getFullYear() + '-' + toDou(oDate1.getMonth() + 1) + '-' + toDou(oDate1.getDate());
 	var leaveDate = oDate2.getFullYear() + '-' + toDou(oDate2.getMonth() + 1) + '-' + toDou(oDate2.getDate());
-	var jd = new Date(), js = new Date(jd.setDate(jd.getDate() + 1)), jr =new Date( jd.setDate(jd.getDate() + 1));
+	var jd = new Date(), js = new Date(jd.setDate(jd.getDate() + 2)), jr =new Date( jd.setDate(jd.getDate() + 1));
+    //H5-1008   T+2~T+3
 	var smonthStr = (js.getMonth()+1)<10?'0'+(js.getMonth()+1):js.getMonth()+1;
 	var sdayStr = js.getDate()<10?'0'+js.getDate():js.getDate();
 	var emonthStr = (jr.getMonth()+1)<10?'0'+(jr.getMonth()+1):jr.getMonth()+1;
