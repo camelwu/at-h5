093c906f3b8ff4419e0de5d763d18e20f78aff0b
zhouwei <zhouwei163mail@163.com>
2016-04-15 13:44:11 +0800

    抽象支付页面

---
+++ payment/js/payment.js
@@ -0,0 +1,88 @@
+/**
+ * Created by zhouwei on 2016/3/31.
+ * 支付页面（酒店，机票，酒+景，机+X）
+ *test ss   sssssssss
+ */
+(function(){
+    var payment=function(){
+        /*支付类型*/
+        var paymentType={
+            "Visa":{id:1,name:"Visa信用卡"},
+            "Mast":{id:2,name:"万事达信用卡"}
+        };
+        var _bussinessType= {
+            "Hotle":{id: 1, name: "酒店", detailCode: "0038", payMentCode: "022"},
+            "Flight":{id: 2, name: "机票", detailCode: "0038", payMentCode: "022"},
+            "Scenic":{id: 3, name: "景点", detailCode: "0095", payMentCode: "0093"},
+            "Tour":{id: 4, name: "酒+景", detailCode: "0038", payMentCode: "022"}
+        };
+
+        var _init={
+          bindPaymentTypeEvent:function(){
+            debugger;
+            alert(111);
+              $(".paymentType b").on("click",function(){
+                 alert(111);
+
+              })
+          }
+
+        }
+        var _getOrderData=function(bussinessType,bookingRefNo,foreEndType,callback){
+            var Parameters={
+                "Parameters": {
+                    "BookingRefNo": bookingRefNo
+                },
+                "ForeEndType":foreEndType,
+                "Code": bussinessType.detailCode
+            }
+            vlm.loadJson("http://10.2.22.239:8888/api/GetServiceApiResult", JSON.stringify(Parameters),callback);
+        };
+        /*支付Modle实体*/
+        var _modle={
+
+        };
+        /*支付方法*/
+        var _paymentEvent=function(paymentType){
+            $.jAlert.confirm("支付完成前，请不要关闭此支付验证窗口 </br> 支付完成后，请根据你支付的情况点击下面的按钮。","网上支付提示",null,"支付完成","支付出现问题");
+            this.data = data;    //post参数对象
+            this.handlePayment = function () {
+                     return  vlm.loadJson("http://10.2.22.239:8888/api/GetServiceApiResult", JSON.stringify(data), function(){
+                         location.href=data.url;
+                });
+            };
+        };
+        /*生成HTML片段*/
+        var _generateHtml=function(type,data){
+            if(type.id==3){
+                var html = template("tpl_scenic_detail", data);
+                $(".pay-type").append(html);
+
+            }
+            else if(type.id==2){
+
+            }
+        }
+        /*页面初始化方法*/
+        var _initPage=function(){
+            var type=_bussinessType[vlm.getpara("type")];
+            var bookingRefNo=vlm.getpara("bookingRefNo");
+            _init.bindPaymentTypeEvent();
+            _getOrderData(type,bookingRefNo,3,function(data){
+                vlm.init();
+                if (data.success) {
+                    _generateHtml(type,data);
+                }
+                else{
+                    jAlert("网络请求错误！");
+                }
+            });
+        };
+        /*接口*/
+        return{
+            InitPage:_initPage()
+        }
+    }()
+
+})()
+
