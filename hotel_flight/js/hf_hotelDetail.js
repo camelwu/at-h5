38bd3310cfb77593fa7a51c118cebc64a9260a19
liushaofei <asdf7896>
2016-04-21 13:45:24 +0800

    Merge branch 'user' of D:\project\at-h5 with conflicts.

--- hotel_flight/js/hf_hotelDetail.js
+++ hotel_flight/js/hf_hotelDetail.js
@@ -1,29 +1,50 @@
-console.log('页面加载成功');
 var data2 = '',roomdata = '';
 (function () {
+    var localJson = eval("localJson="+localStorage.getItem("hotelDetailInfo"));
+    console.log(localJson)
+    //var data = {
+    //    "Code":"50100009",
+    //    "ForeEndType":2,
+    //    "Parameters":{
+    //        "HotelID":"11911",
+    //        "SelectedRoomID":173151,
+    //        "FlightCacheID":"1013219",
+    //        "FlightSetID":"1003003",
+    //        "CityCodeFrom":"SIN",
+    //        "CityCodeTo":"BKK",
+    //        "DepartDate":"2016-05-07",
+    //        "ReturnDate":"2016-05-08",
+    //        "RoomDetails":[
+    //            {"Adult":2}
+    //        ]
+    //    }
+    //}
     var data = {
         "Code":"50100009",
         "ForeEndType":2,
         "Parameters":{
-            "HotelID":"11911",
-            "SelectedRoomID":173151,
-            "FlightCacheID":"1013219",
-            "FlightSetID":"1003003",
-            "CityCodeFrom":"SIN",
-            "CityCodeTo":"BKK",
-            "DepartDate":"2016-05-07",
-            "ReturnDate":"2016-05-08",
-            "RoomDetails":[
-                {"Adult":2}
+            "HotelID": 1005455,
+            "SelectedRoomID": "",
+            "FlightCacheID": 14094,
+            "FlightSetID": 1001001,
+            "CityCodeFrom": "SIN",
+            "CityCodeTo": "BKK",
+            "DepartDate": "2016-05-17T16:00:00",
+            "ReturnDate": "2016-05-25T09:40:00",
+            "RoomDetails": [
+                {
+                    "adult": 2
+                }
             ]
         }
     }
 
+
     vlm.loadJson('', JSON.stringify(data), dataCallBack);//url统一改vlm中的，此处可以为空
     function dataCallBack(result) {
-        console.log(result);
+        //console.log(result);
         data2 = result.data;
-        console.log(data2);
+        //console.log(data2);
         roomdata = data2.hotelInfo.rooms;
         //console.log(roomdata[0].roomName);
         nav();
@@ -43,26 +64,22 @@ var data2 = '',roomdata = '';
         $('.jhf-banner').html(banner);
     }
     //日历部分
-
-
-    rili();
-    function rili(){
-        var dateInitObj = new Object();
-        var myDate2 = new Calender({
-            id : 'chooseDate',
-            num : 13,
-            time : dateInitObj,
-            sClass1 : 'enterDate',
-            id2 : 'nightNum',
-            fn : upDateContent
-        });
-        function upDateContent(){
-            alert(222)
-            hotelDetail.gdataInfo.CheckInDate = document.getElementsByClassName('enterDate')[0].innerHTML;
-            hotelDetail.gdataInfo.CheckOutDate = document.getElementsByClassName('enterDate')[1].innerHTML;
-            hotelDetail.init(hotelDetail.gdataInfo);
-        }
-    }
+    //
+    //
+    //rili();
+    //function rili(){
+    //    var dateInitObj = new Object();
+    //    var myDate2 = new Calender({
+    //        id : 'chooseDate',
+    //        num : 13,
+    //        time : dateInitObj,
+    //        sClass1 : 'enterDate',
+    //        id2 : 'nightNum',
+    //        fn : upDateContent
+    //    });
+    //    function upDateContent(){
+    //    }
+    //}
     //地址 星级 wifi
     function adress(){
         var jhf_score = template("jhf_score", data2.hotelInfo);
@@ -78,6 +95,7 @@ var data2 = '',roomdata = '';
             $('.jhf-mes li.showh .slide').eq(i).click(function(){
                 $(this).find('i').toggleClass('cur');
                 $('.jhf-mes ol.show').eq(i).slideToggle();
+                $(this).find('b').toggleClass('cur').parents('li.showh').siblings().find('b').removeClass('cur');
             })
         });
     }
