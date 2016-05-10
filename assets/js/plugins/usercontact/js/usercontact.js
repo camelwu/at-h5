5e446db73ded2f28c85419cbc142235f16d41511
zhouwei <zhouwei163mail@163.com>
2016-05-10 13:30:32 +0800

    支付页面样式调整

---
+++ assets/js/plugins/usercontact/js/usercontact.js
@@ -0,0 +1,18 @@
+/**
+ * Created by zhouwei on 2016/5/9.
+ */
+/**
+ * Created by zhouwei on 2016/5/4.
+ */
+define(['jquery'],function (){
+
+   debugger;
+  var init=function(){
+
+  }
+
+  return {
+    init: init
+  };
+
+})
---
+++ assets/plugins/usecontact/js/usercontact.js
@@ -0,0 +1,18 @@
+/**
+ * Created by zhouwei on 2016/5/9.
+ */
+/**
+ * Created by zhouwei on 2016/5/4.
+ */
+define(['jquery',"ejs_server","vlm"],function (){
+
+
+  var init=function(){
+
+  }
+
+  return {
+    init: init
+  };
+
+})
