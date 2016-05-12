abcd0d8241fe55e65bdc6b912b0896e88228d6c4
liushaofei <asdf7896>
2016-05-12 17:35:49 +0800

    机+酒+景  酒店列表数据串连

--- assets/hotel_flight_tour/js/hft_hotelDetail.js
+++ assets/hotel_flight_tour/js/hft_hotelDetail.js
@@ -4,6 +4,8 @@
   //console.log(sStorage);
   //取url再传给资源选择页
   var chooseUrl = sessionStorage.getItem("hftHotelChooseUrl");
+  //添加当前选中样式  资源选择页和列表页都有roomId，所以不用判断
+  var ulrRoomId = parseInt(chooseUrl.substring(15));
 
   //经纬度
   var latitude = 0;
@@ -68,6 +70,13 @@
     var str = $('#ulList').html();
     var ulList = ejs.render(str,data.hotelInfo);
     $('ul.ul_room').html(ulList);
+    //添加当前选中
+    $('.hotel_detail_rooms li').each(function(i){
+      //if(ulrRoomId == data.hotelInfo.rooms[i].roomID){
+      //  $('.hotel_detail_rooms li').eq(i).addClass('cur');
+      //}
+    })
+    //点击事件  跳转
     $('.hotel_detail_rooms li').on('click',function(){
       var roomID = $(this).attr('data-hotelId');
       $(this).addClass('cur').siblings().removeClass('cur');
