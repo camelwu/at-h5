b2c8bc5b1f7cd52dc6d189122fb589a113decc75
chaimeili <li4225861>
2016-05-12 15:32:47 +0800

    景点列表页

--- assets/hotel_flight_tour/js/hft_scenic_content.js
+++ assets/hotel_flight_tour/js/hft_scenic_content.js
@@ -4,6 +4,7 @@
 (function(){
     var webkit = this;
     var tpl1 = [
+        '<span class="bar_img_theme">景点详情</span>',
         '<% if(images.length==0){ %>',
         '<img src="<%= pictureURL%>" alt="image"/>',
         '<% }else{ %>' +
@@ -23,7 +24,7 @@
         var ContentList = function(){
             var packageID=vlm.getpara("packageID");
             var cityCodeFrom=vlm.getpara("cityCodeFrom");
-            var param=
+            var Cparam=
             {
                 "Parameters": {
                     "PackageID": packageID,
@@ -32,47 +33,48 @@
                 "ForeEndType": 2,
                 "Code": "60100003"
             };
-            vlm.loadJson("",JSON.stringify(param),function(data){
-                console.log(JSON.stringify(param));
-                if(data.success){
-                    console.log(data);
-                    var htmlc = $("#Barcontent").html();
-                    var htmlC = ejs.render(htmlc,data.data);
-                    $("#barContent").html(htmlC);
-                    var htmls = $("#Sceniccontent").html();
-                    var htmlS = ejs.render(htmls,data.data);
-                    //图片点击事件
-                    var htmlB = ejs.render(tpl1,data.data);
-                    $("#barImg").html(htmlB);
-                    $(".swipebox").click(function() {
-                        $('.gallery').hide(0);
-                        $('.portfolio-wide').hide(0);
-                    });
-                    $(".swipebox").swipebox({
-                        useCSS : true,
-                        hideBarsDelay : 0
-                    });
-                    //加载更多点击事件
-                    $("#scenicContent").html(htmlS);
-                    var Sheight1=$("#Sheight1")[0];
-                    var Sheight2=$("#Sheight2")[0];
-                    var Sheight3=$("#Sheight3")[0];
-                    Sheight1.onclick =function(){
-                        $(".scenic_height1").css({'height':'100%'});
-                        $("#Sheight1").css({'display':'none'});
-                    };
-                    Sheight2.onclick =function(){
-                        $(".scenic_height2").css({'height':'100%'});
-                        $("#Sheight2").css({'display':'none'});
-                    };
-                    Sheight3.onclick =function(){
-                        $(".scenic_height3").css({'height':'100%'});
-                        $("#Sheight3").css({'display':'none'});
-                    };
-                }else {
-                    jAlert(json.message,"提示");
-                }
-            });
+            vlm.loadJson("",JSON.stringify(Cparam),callback);
+        };
+        var callback = function(data){
+            vlm.init();
+            if(data.success){
+                console.log(data);
+                var htmlc = $("#Barcontent").html();
+                var htmlC = ejs.render(htmlc,data.data);
+                $("#barContent").html(htmlC);
+                var htmls = $("#Sceniccontent").html();
+                var htmlS = ejs.render(htmls,data.data);
+                //图片点击事件
+                var htmlB = ejs.render(tpl1,data.data);
+                $("#barImg").html(htmlB);
+                $(".swipebox").click(function() {
+                    $('.gallery').hide(0);
+                    $('.portfolio-wide').hide(0);
+                });
+                $(".swipebox").swipebox({
+                    useCSS : true,
+                    hideBarsDelay : 0
+                });
+                //加载更多点击事件
+                $("#scenicContent").html(htmlS);
+                var Sheight1=$("#Sheight1")[0];
+                var Sheight2=$("#Sheight2")[0];
+                var Sheight3=$("#Sheight3")[0];
+                Sheight1.onclick =function(){
+                    $(".scenic_height1").css({'height':'100%'});
+                    $("#Sheight1").css({'display':'none'});
+                };
+                Sheight2.onclick =function(){
+                    $(".scenic_height2").css({'height':'100%'});
+                    $("#Sheight2").css({'display':'none'});
+                };
+                Sheight3.onclick =function(){
+                    $(".scenic_height3").css({'height':'100%'});
+                    $("#Sheight3").css({'display':'none'});
+                };
+            }else {
+                jAlert(json.message,"提示");
+            }
         };
         return {
             ContentList:ContentList
