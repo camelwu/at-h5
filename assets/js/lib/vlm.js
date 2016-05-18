1b38f0e2e6434afb3bac203c142c0075da50a63c
qizhenzhen@asiatravel.com <haiou123456>
2016-05-18 17:33:10 +0800

    用户中心 user页rem   替换

--- assets/js/lib/vlm.js
+++ assets/js/lib/vlm.js
@@ -12,7 +12,6 @@
 			var _api = "http://10.7.2.111/api/GetServiceApiResult", lStorage = window.localStorage, sStorage = window.sessionStorage, basePath = basePath == undefined ? "http://" + window.location.host : basePath, menus = {
 
 				home : ['首页', basePath],
-				find : ['目的地', basePath + '/building.html'],
 				user : ['我的', basePath + '/user/user.html']
 			},  _codeDic={
 				//1.酒店(10)
