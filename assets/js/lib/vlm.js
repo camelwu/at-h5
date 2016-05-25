d915205a690d57bb19a18a22fe8499c39a362682
chaimeili <li4225861>
2016-05-25 11:28:52 +0800

    酒店

--- assets/js/lib/vlm.js
+++ assets/js/lib/vlm.js
@@ -9,7 +9,7 @@
 (function(e, t) {
 	var n = n || (function(n) {//123.56.190.34:8888 //10.6.11.20:8888 10.7.2.111
 
-			var _api = "http://10.7.2.1/api/GetServiceApiResult", lStorage = window.localStorage, sStorage = window.sessionStorage, basePath = basePath == undefined ? "http://" + window.location.host : basePath, menus = {
+			var _api = "http://10.7.2.111/api/GetServiceApiResult", lStorage = window.localStorage, sStorage = window.sessionStorage, basePath = basePath == undefined ? "http://" + window.location.host : basePath, menus = {
 
 				home : ['首页', basePath],
 				user : ['我的', basePath + '/user/user.html']
