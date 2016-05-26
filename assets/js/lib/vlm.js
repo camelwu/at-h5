dcc50bd23ed2227bef43a006704ebbbd09d472e6
zhouwei <zhouwei163mail@163.com>
2016-05-26 17:14:45 +0800

    重构用户选择模块模块

--- assets/js/lib/vlm.js
+++ assets/js/lib/vlm.js
@@ -9,8 +9,8 @@
 
 (function (e, t) {
     var n = n || (function (n) { //123.56.190.34:8888 //10.6.11.20:8888 10.7.2.111
-        var _api = "http://10.7.2.119/api/GetServiceApiResult",
-            _apiWithDeviceID = "10.7.2.119/apiWihtDeviceID/GetServiceApiResult",
+        var _api = "http://10.7.2.111/api/GetServiceApiResult",
+            _apiWithDeviceID = "http://10.7.2.111/apiWihtDeviceID/GetServiceApiResult",
             lStorage = window.localStorage,
             sStorage = window.sessionStorage,
             basePath = basePath == undefined ? "http://" + window.location.host : basePath,
@@ -112,7 +112,7 @@
                 //7. 短信(90)
                 "001301": "90100001"
             }
-        arr_t = {
+             arr_t = {
                 1: '护照',
                 2: '身份证',
                 3: '出生证明',
@@ -364,9 +364,14 @@
                         return "周" + reslut;
                     }
                 },
-                  getAgeByBrithday:function GetAgeByBrithday(birthday,today){
-                    var age=-1;
-                    var today=new Date(departDate.replace(/(\d{4})-(\d{2})-(\d{2})T(.*)?\.(.*)/, "$1/$2/$3 $4"));
+               getAge:function GetAgeByBrithday(birthday,departDate){
+                    var age=-1,today;
+                    if(departDate==undefined){
+                      today=new Date();
+                    }
+                     else {
+                      today = new Date(departDate.replace(/(\d{4})-(\d{2})-(\d{2})T(.*)?\.(.*)/, "$1/$2/$3 $4"));
+                    }
                     var todayYear=today.getFullYear();
                     var todayMonth=today.getMonth()+1;
                     var todayDay=today.getDate();
