07e58691e2d6d11765fc109d35336aac9416f23e
lichengjun <taoxirou@163.com>
2016-05-26 14:11:04 +0800

    机+酒+景相关页面非js生成部分节点埋点 删除无用文件

--- assets/js/lib/vlm.js
+++ assets/js/lib/vlm.js
@@ -10,7 +10,7 @@
 (function (e, t) {
     var n = n || (function (n) { //123.56.190.34:8888 //10.6.11.20:8888 10.7.2.111
         var _api = "http://10.7.2.119/api/GetServiceApiResult",
-            _apiWithDeviceID = "http://127.0.0.1/apiWihtDeviceID/GetServiceApiResult",
+            _apiWithDeviceID = "http://127.0.0.1:1337/apiWihtDeviceID/GetServiceApiResult",
             lStorage = window.localStorage,
             sStorage = window.sessionStorage,
             basePath = basePath == undefined ? "http://" + window.location.host : basePath,
@@ -129,19 +129,22 @@
                     return decodeURIComponent(r[2]);
                 return null;
             }, setUrlPara = function (url, key, value) {
-          var originalUrl = url ? url : window.location.href , newUrl = "",reg = new RegExp(key + "=([^&]*)(&|$)"),result =[] ;
-          newUrl = originalUrl;
-          result = newUrl.match(reg);
-          if(result){
-            if(!value){
-              return  newUrl.replace(result[0], "")
-            }else{
-              return  newUrl.replace(result[0], key + "=" + value + result[2])
-            }
-          }else{
-            return newUrl.indexOf('?')>-1?newUrl+"&"+key+"="+value:newUrl+"?"+key+"="+value
-          }
-			}, parseUrlPara = function(url, isEncode) {
+                var originalUrl = url ? url : window.location.href,
+                    newUrl = "",
+                    reg = new RegExp(key + "=([^&]*)(&|$)"),
+                    result = [];
+                newUrl = originalUrl;
+                result = newUrl.match(reg);
+                if (result) {
+                    if (!value) {
+                        return newUrl.replace(result[0], "")
+                    } else {
+                        return newUrl.replace(result[0], key + "=" + value + result[2])
+                    }
+                } else {
+                    return newUrl.indexOf('?') > -1 ? newUrl + "&" + key + "=" + value : newUrl + "?" + key + "=" + value
+                }
+            }, parseUrlPara = function (url, isEncode) {
                 var isEncode = isEncode || false;
                 var reg = /([^=&?]+)=([^=&?]+)/g,
                     obj = {};
@@ -535,7 +538,7 @@
                         //var pattern = /^[a-zA-Z0-9-_]{4,20}$/;
                         var pattern1 = /^[\u4E00-\u9FA5a-zA-Z0-9][\u4E00-\u9FA5a-zA-Z0-9_]{3,19}$/;
                         var pattern2 = /^[\u4E00-\u9FA5]{2,10}$/;
-						var pattern3 = /^[\u4E00-\u9FA5][\u4E00-\u9FA5a-zA-Z0-9_]{2,18}$/;
+                        var pattern3 = /^[\u4E00-\u9FA5][\u4E00-\u9FA5a-zA-Z0-9_]{2,18}$/;
                         //4-20个字符，可由中英文字母，数字、"_"组成，不能以'_'开头
                         if (pattern1.test(name) || pattern2.test(name) || pattern3.test(name)) {
                             return true;
