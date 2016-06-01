4406a26937972181a7d10938f9b030b06ac1def2
liushaofei <asdf7896>
2016-06-01 16:40:05 +0800

    酒景  景点列表添加无数据显示

--- assets/tour/js/scenicList.js
+++ assets/tour/js/scenicList.js
@@ -40,7 +40,7 @@
 		}
 		vlm.loadJson(apiurl, JSON.stringify(initdata), function(data) {
 			var json = data, tpl_page = '<div id="loadMore">点击查看更多...</div>', tpl_end = '<div id="loadMore">没有更多...</div>';
-			if (json.success) {
+			if (json.success && json.success.data) {
 				var data = json.data, items = [], themes = data.themes, fts = {
 					allowMultiSelect : 0,
 					filterType : 5,
@@ -103,7 +103,8 @@
 					footer.filters.init();
 				}
 			} else {
-				jAlert(json.message, "提示");
+				$('.amy_error_box').show();
+				//jAlert(json.message, "提示");
 			}
 		});
 	}).call(this, 1);
