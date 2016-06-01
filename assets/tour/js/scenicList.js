975c8c866372a12935b74025603062ed45ac66b0
liushaofei <asdf7896>
2016-06-01 17:33:25 +0800

    酒景  去掉景点列表的分页文字

--- assets/tour/js/scenicList.js
+++ assets/tour/js/scenicList.js
@@ -39,8 +39,10 @@
 			}
 		}
 		vlm.loadJson(apiurl, JSON.stringify(initdata), function(data) {
-			var json = data, tpl_page = '<div id="loadMore">点击查看更多...</div>', tpl_end = '<div id="loadMore">没有更多...</div>';
+			//tpl_page = '<div id="loadMore">点击查看更多...</div>'
+			var json = data, tpl_page = '', tpl_end = '<div id="loadMore">没有更多...</div>';
 			if (json.success && json.data != null) {
+				console.log(json)
 				var data = json.data, items = [], themes = data.themes, fts = {
 					allowMultiSelect : 0,
 					filterType : 5,
