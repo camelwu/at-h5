80f0d4ca01eb0a6b1750a8db8b5a5ffbb5ef20e1
liushaofei <asdf7896>
2016-06-01 16:47:45 +0800

    酒景  景点列表添加无数据显示    列表页添加图片加载失败图片

--- assets/tour/js/scenicList.js
+++ assets/tour/js/scenicList.js
@@ -40,7 +40,7 @@
 		}
 		vlm.loadJson(apiurl, JSON.stringify(initdata), function(data) {
 			var json = data, tpl_page = '<div id="loadMore">点击查看更多...</div>', tpl_end = '<div id="loadMore">没有更多...</div>';
-			if (json.success && json.success.data) {
+			if (json.success && json.data != null) {
 				var data = json.data, items = [], themes = data.themes, fts = {
 					allowMultiSelect : 0,
 					filterType : 5,
