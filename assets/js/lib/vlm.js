d3d9d2f7d993ff429491c4a04d56bcdadda1fce7
zhouwei <zhouwei163mail@163.com>
2016-06-06 09:59:46 +0800

    修改node服务层访问119

--- assets/js/lib/vlm.js
+++ assets/js/lib/vlm.js
@@ -9,8 +9,8 @@
 
 (function (e, t) {
     var n = n || (function (n) { //123.56.190.34:8888 //10.6.11.20:8888 10.7.2.111  10.7.2.119
-        var _api = "http://10.7.2.111/api/GetServiceApiResult",
-            _apiWithDeviceID = "http://10.7.2.111/apiWihtDeviceID/GetServiceApiResult",
+        var _api = "http://10.7.2.119/api/GetServiceApiResult",
+            _apiWithDeviceID = "http://10.7.2.119/apiWihtDeviceID/GetServiceApiResult",
             lStorage = window.localStorage,
             sStorage = window.sessionStorage,
             basePath = basePath == undefined ? "http://" + window.location.host : basePath,
