4bcf5b1c6a1ff17a26eece88e4ccfaf2092ca149
liushaofei <asdf7896>
2016-06-13 18:12:28 +0800

    FHT 酒店详情页面制作，和app核对酒店特色数据

---
+++ assets/hotel_flight_tour/js/hft_hotelSummary.js
@@ -0,0 +1,27 @@
+window.onload = function() {
+	var data = JSON.parse(sessionStorage.getItem("fhtHotelCharacteristic"));
+	console.log(data)
+	var hotelDuse = data.hotelDesc;
+	$('.hotel_content1').html(hotelDuse);
+	//获取参数
+	vlm.loadJson("", JSON.stringify(data), dataCallBack);
+	function dataCallBack(result){
+		console.log(result)
+		if (result.success && result.code==200) {
+			var data = result.data;
+
+			var ulHtml = "";
+			for(var i=0;i<data.length;i++){
+				ulHtml += '<li class="ul2-li"><b class="s-icon3 hotel_feature_li_icon" style="margin-bottom:0px"></b><p class="s-p4 hotel_feature_li_p">'+data[i].featureDesc+'</p></li>';
+			}
+			console.log(ulHtml)
+			$('.hotel_feature').html(ulHtml);
+
+
+			vlm.init();
+		} else {
+			vlm.init();
+			$(".content").html("暂无酒店特色信息");
+		}
+	};
+};
