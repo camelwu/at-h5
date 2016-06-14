c7e5e4ef1d5095a00463a8266522f873e36774d8
wusongbo <wusongbo@asiatravel.com>
2016-06-14 20:47:08 +0800

    参数都没写全，写的什么东西。重构了。

--- assets/hotel_flight_tour/js/hft_scenic_list.js
+++ assets/hotel_flight_tour/js/hft_scenic_list.js
@@ -5,47 +5,39 @@
 	var webkit = this;
 	var filterSign = false;
 	var core = function() {
-		var url = "";
-		var themeId = "";
-		//获取附近城市列表
+		//获取缓存内容
+		var searchInfo = JSON.parse(localStorage.getItem("searchInfo")),
+		//全参设定
+		SParameter = {
+			"Parameters" : {
+				"departCityCode" : searchInfo.FromCity,
+				"destCityCode" : searchInfo.ToCity,
+				"departDate" : searchInfo.DepartDate,
+				"returnDate" : searchInfo.ReturnDate,
+				// Android Request Arguments
+				"roomDetails" : searchInfo.RoomInfo,
+				"FilterFields" : [],
+				"sortType" : 0,
+				"PageIndex" : "1",
+				"pageSize" : "20"
+			},
+			"ForeEndType" : 3,
+			"Code" : "60100002"
+		};
+		console.log(JSON.stringify(SParameter));
 		//获取景点列表
 		var ScenicList = function() {
-      var searchInfo = JSON.parse(localStorage.getItem("searchInfo"))
-			var destCityCode = searchInfo.FromCity;
-			var departCityCode = searchInfo.ToCity;
-			var departDate = searchInfo.DepartDate;
-			var returnDate = searchInfo.ReturnDate;
-			var AdultNum = searchInfo.AdultNum;
-			var SParameter = {
-				"Parameters" : {
-					"departCityCode" : destCityCode,
-					"destCityCode" : departCityCode,
-					"departDate" : departDate,
-					"returnDate" : returnDate,
-          // Android Request Arguments
-          // "pageIndex":"1",
-          // "pageSize":"20",
-          "roomDetails": [{
-            "adult": AdultNum
-          }],
-          "sortType":0
-        },
-				"ForeEndType" : 3,
-				"Code" : "60100002"
-			};
-			console.log(JSON.stringify(SParameter));
-			vlm.loadJson(url, JSON.stringify(SParameter), callback);
-		};
-		var callback = function(data) {
+			vlm.loadJson('', JSON.stringify(SParameter), callback);
+		},
+		//全局回调处理
+		callback = function(data) {
 			if (data.success) {
-        		vlm.init();
+				vlm.init();
 				filterData = data;
-				var localStoragedata = JSON.parse(localStorage.getItem("searchInfo"));
-				console.log(data);
 				//var htmlT = ;
 				$("#tour_city").html(ejs.render($("#tpl_head").html(), data.data));
 				var htmlt = $("#timeDetile").html();
-				var htmlT = ejs.render(htmlt, localStoragedata);
+				var htmlT = ejs.render(htmlt, searchInfo);
 				$("#TimeList").html(htmlT);
 				var htmlp = $("#scenicDetile").html();
 				var html = ejs.render(htmlp, data.data);
@@ -64,10 +56,10 @@
 					var packageid = $(this).attr("data-packageid");
 					window.location.href = 'hft_choose.html?type=2&packageId=' + packageid;
 				});
-				$('#Time').on("click",function() {
+				$('#Time').on("click", function() {
 					window.location.href = 'index.html';
 				});
-				if(!filterSign){
+				if (!filterSign) {
 					filterSign = true;
 					initFilter(data);
 				}
@@ -76,81 +68,57 @@
 			}
 		};
 		//init filter
-		var initFilter = function(data){
+		var initFilter = function(data) {
 			// 添加底部筛选
-			var f_data = {
-				sortTypes : {
-					title : "快速排序",
-					c : "foot_sort",
-					s : 1,
-					type : 1,
-					key : 'sortTypes',
-					listData : data.data.sortTypes
-				},
-				hotelScreen : {
-					title : "筛选",
-					c : "foot_screen",
-					s : 1,
-					type : 2,
-					key : 'filters',
-					listData : data.data.filters
-				}
-			},menu_call = function(obj) {
-				var Para={};
-				var departCityCode = JSON.parse(localStorage.getItem("searchInfo")).FromCity;
-				var destCityCode = JSON.parse(localStorage.getItem("searchInfo")).ToCity;
-				Para.departCityCode = departCityCode;
-				Para.destCityCode = destCityCode;
-				Para.sortType = obj.sortTypes[0];
-				Para.filterFields = obj.filters;
-				tAjax("",Para,"60100002","3",callback);
-			};
 			if (footer) {
-				footer.data = f_data;
-				footer.callback = menu_call;
-			}
+				footer.data = {
+					sortTypes : {
+						title : "快速排序",
+						c : "foot_sort",
+						s : 1,
+						type : 1,
+						key : 'sortTypes',
+						listData : data.data.sortTypes
+					},
+					hotelScreen : {
+						title : "筛选",
+						c : "foot_screen",
+						s : 1,
+						type : 2,
+						key : 'filters',
+						listData : data.data.filters
+					}
+				};
+				footer.callback = function(obj) {
+					SParameter.Parameters.sortType = obj.sortTypes[0];
+					SParameter.Parameters.filterFields = obj.filters;
+					ScenicList();
+				};
 				footer.filters.init();
-		};
-		//ajax请求
-		var tAjax= function(questUrl, data, Code, ForeEndType, Callback) {
-			var that = this, dataObj = {
-				Parameters : data,
-				ForeEndType : ForeEndType,
-				Code : Code
-			};
-			//questUrl = questUrl || that.requestUrl;
-			vlm.loadJson("", JSON.stringify(dataObj), Callback);
+			}
 		};
 		//城市列表父宽
 		var PWidth = function(data) {
-      var sum = data.data.recommendCities.length;
-      var num=0;
-      var width =0;
-      for (var i = 0; i < sum; i++) {
-        width= $(".city_list1").eq(i).width();
-        console.log(width);
-        num += width;
-      }
-      num = num + sum*32;
-      console.log(num);
+			var sum = data.data.recommendCities.length;
+			var num = 0;
+			var width = 0;
+			for (var i = 0; i < sum; i++) {
+				width = $(".city_list1").eq(i).width();
+				console.log(width);
+				num += width;
+			}
+			num = num + sum * 32;
+			console.log(num);
 			$(".city_box").css({
-				'width' : num+ 'px'
+				'width' : num + 'px'
 			});
 		};
 		return {
 			ScenicList : ScenicList
 		};
 	}();
-	webkit.vlm = webkit.vlm || {};
-	webkit.vlm.load = function() {
-		core.ScenicList();
-    window.sessionStorage.removeItem('hftFlightHotelTourInfo'); //清空历史数据
-	};
+	core.ScenicList();
+	//清空历史数据
+	window.sessionStorage.removeItem('hftFlightHotelTourInfo');
 })();
-vlm.load();
-//城市返回记录已访问过
-//$('#g_back').click(function() {
-//	var isInit = window.location.search;
-//	window.location.href = 'index.html' + isInit;
-//});
 
