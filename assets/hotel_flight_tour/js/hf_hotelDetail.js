40b2233fae3487d3cc3370b38946d65b3739ddb1
chaimeili <li4225861>
2016-06-22 11:05:40 +0800

    机+酒+景，酒店详情

--- assets/hotel_flight_tour/js/hf_hotelDetail.js
+++ assets/hotel_flight_tour/js/hf_hotelDetail.js
@@ -179,8 +179,8 @@ var data2 = '', roomdata = '';
 })();
 (function(){
   $(window)[0].addEventListener("scroll",function(){
-    var header = $(".header")[0];
-    if(!$(".all_elements").scrollTop == 0){
+    var header = $(".hft_header")[0];
+    if(!$(".contents").scrollTop == 0){
       header.style.position="fixed";
       header.style.opacity="1";
       $("#oldHeader")[0].style.opacity = "0";
