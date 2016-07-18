5e95da7c037f1bac729a3d5926139f7e0a7e7a3a
Jason <taoxirou@163.com>
2016-07-18 10:08:44 +0800

    ¾²̬hash·¢²¼²âÊÔ

---
+++ assets/deploytest/js/test.index.js
@@ -0,0 +1,6 @@
+/**
+ * Created by lichengjun on 16/7/15.
+ */
+(function(){
+  console.info('deploy test copy');
+})();
---
+++ tasks/register/filehash.js
@@ -0,0 +1,21 @@
+/**
+ * Created by lichengjun on 16/7/15.
+ */
+/**
+ * `file hash`
+ *
+ * ---------------------------------------------------------------
+ *
+ * 对静态文件做HASH值计算   最大限度利用缓存
+ *
+ */
+module.exports = function (grunt) {
+  grunt.registerTask('filehash', [
+    'linkAssets',
+    'compileAssets',
+    'uglify:dev',
+    'cssmin:dev',
+    'clean:demo', //删除不能访问的资源
+    'clean:plugins' //删除不能访问的资源
+  ]);
+};
