b9a6d836b922032b17f28132bc17941aa2ce2523
qizhenzhen <qizhenzhen@asiatravel.com>
2016-07-20 17:26:32 +0800

    一元门票模拟数据2

--- assets/oneTicket/js/oneticket.js
+++ assets/oneTicket/js/oneticket.js
@@ -16,54 +16,87 @@
       "ForeEndType": 3,
       "Code": "80100004"
     }
+    function send_req_tab(tmp){
 
-    //console.log(Parmeters);
-    vlm.loadJson("", JSON.stringify(Parmeters), saleList_back);
+      //console.log(Parmeters);
+      vlm.loadJson("", JSON.stringify(tmp), saleList_back);
 
-    function saleList_back(ret) {
-      var json = ret;
-      console.log(json);
-      if (json.success) {
-        //特卖模块
-        var greatSaleData = json.data.hotScenicList;
-        var greatSalestr = $('#greatSale').html();
-        var greatSaleList = ejs.render(greatSalestr, {greatSaleData: greatSaleData})
-        $('#one_sale_cont').html(greatSaleList);
+      function saleList_back(ret) {
+        var json = ret;
+        console.log(json);
+        if (json.success) {
+          //特卖模块
+          var greatSaleData = json.data.hotScenicList;
+          var greatSalestr = $('#greatSale').html();
+          var greatSaleList = ejs.render(greatSalestr, {greatSaleData: greatSaleData})
+          $('#one_sale_cont').html(greatSaleList);
+          def_img();
 
-        def_img();
-
-      } else {
-        jAlert(json.message);
+        } else {
+          jAlert(json.message);
+        }
       }
     }
+    send_req_tab(Parmeters);
+
+    //Tab切换
+    $('.one_select >li').on('click', function () {
+      var one_index = $(this).index();
+      $(this).addClass('active').siblings().removeClass('active');
+      send_req_tab(Parmeters);
+      switch (one_index) {
+        case 0:
+          $('.shortexhibition').attr('src','../../images/oneticket/great_sale.png');
+          $('.one_sale_cont').css('backgroundColor','#fed81d');
+          $('.one_foot').css('backgroundColor','#fed81d');
+          break;
+        case 1:
+          $('.shortexhibition').attr('src','../../images/oneticket/child_all.png');
+          $('.one_sale_cont').css('backgroundColor','#7bc300');
+          $('.one_foot').css('backgroundColor','#7bc300');
+          break;
+        case 2:
+          $('.shortexhibition').attr('src','../../images/oneticket/bestie.png');
+          $('.one_sale_cont').css('backgroundColor','#65ebe0');
+          $('.one_foot').css('backgroundColor','#65ebe0');
+          break;
+        case 3:
+          $('.shortexhibition').attr('src','../../images/oneticket/lovers.png');
+          $('.one_sale_cont').css('backgroundColor','#4ad1ef');
+          $('.one_foot').css('backgroundColor','#4ad1ef');
+          break;
+      }
+
+
+    });
 
   }
 
   init();
 
   //默认图片
-  function def_img(){
+  function def_img() {
     //默认图片
     var images = $("#one_sale_cont").find('img');
     var error_url = '../../images/hotelDetailerrorpic.png';
     for (var i = 0; i < images.length; i++) {
-      (function(index) {
+      (function (index) {
         var re_url = images[i].getAttribute('data-src');
-        loadImage(re_url, error_url, function() {
+        loadImage(re_url, error_url, function () {
           images[index].setAttribute('src', re_url);
 
-        }, function() {
+        }, function () {
           images[index].setAttribute('src', error_url);
         });
 
         function loadImage(url, error_url, callback, errorFunc) {
           var img = new Image();
           img.src = url;
-          img.onload = function() {
+          img.onload = function () {
             img.onload = null;
             callback();
           };
-          img.onerror = function() {
+          img.onerror = function () {
             img.onerror = null;
             errorFunc();
           }
@@ -71,12 +104,6 @@
       })(i);
     }
   }
-
-  //Tab切换
-  $('.one_select >li').on('click', function () {
-    $(this).addClass('active').siblings().removeClass('active');
-    $('.one_travel_wrap >div').eq($(this).index()).addClass('on').siblings().removeClass('on');
-  });
 
 
 })();
